822aeb3e97e9a8dbc169b6929a4172644116d3f8
Acrom <zvez.dmtr@gmail.com>
2020-10-07 20:32:27 +1000

    Working version of tree builder

--- src/Tree.ts
+++ src/Tree.ts
@@ -1,4 +1,4 @@
-import {Node} from './Node'
+import {Node, NodeType} from './Node'
 import {operators} from './types'
 
 export class Tree {
@@ -13,35 +13,58 @@ export class Tree {
         this.gen(this.tokens, this.root)
     }
 
-    gen(tokens: string[], root: Node) {
+    gen(tokens: string[], root: Node): void {
+        if (tokens.length === 1) {
+            root.value = tokens[0]
+            root.type = NodeType.Number
+            return
+        }
+        const lowPriorityIndex = this.getLowPriorityToken(tokens)
+        // Если нет оператора вне скобок
+        if (lowPriorityIndex === -1) {
+            this.gen(tokens.slice(1, -1), root)
+            return
+        }
+        root.value = tokens[lowPriorityIndex]
+        root.type = NodeType.Operator
+        root.left = new Node()
+        this.gen(tokens.slice(0, lowPriorityIndex), root.left)
+        root.right = new Node()
+        this.gen(tokens.slice(lowPriorityIndex + 1), root.right)
+    }
+
+    // Возвращает приоритет оператора: чем больше возращаемое значение - тем больше приоритет
+    getPriority = (operator: string): number => {
         // Сгруппированы в порядке возрастания приоритета
         const operatorsPriority = [['+', '-'], ['*', '/']]
-        // Возвращает приоритет оператора: чем больше возращаемое значение - тем больше приоритет
-        const getPriority = (operator: string): number => {
-            for (let i = 0; i < operatorsPriority.length; ++i) {
-                if (operatorsPriority[i].includes(operator)) {
-                    return i
-                }
+        for (let i = 0; i < operatorsPriority.length; ++i) {
+            if (operatorsPriority[i].includes(operator)) {
+                return i
             }
-            return -1
         }
-        // Возвращает индекс оператора с наименьшим приоритетом
-        const getLowPriorityToken = (tokensArray: string[]): number => {
-            let lowPriority = Number.MAX_VALUE
-            let index = -1
-            for (let i = 0; i < tokensArray.length; ++i) {
-                const token = tokensArray[i]
-                if (operators.includes(token)) {
-                    const tokenPriority = getPriority(token)
-                    if (tokenPriority < lowPriority) {
-                        lowPriority = tokenPriority
-                        index = i
-                    }
-                }
-            }
-            return index
-        }
+        return -1
+    }
 
+    // Возвращает индекс оператора с наименьшим приоритетом
+    getLowPriorityToken = (tokensArray: string[]): number => {
+        let lowPriority = Number.MAX_VALUE
+        let index = -1
+        let parenthesesCount = 0
+        for (let i = 0; i < tokensArray.length; ++i) {
+            const token = tokensArray[i]
+            if (operators.includes(token) && parenthesesCount === 0) {
+                const tokenPriority = this.getPriority(token)
+                if (tokenPriority < lowPriority) {
+                    lowPriority = tokenPriority
+                    index = i
+                }
+            } else if (token === '(') {
+                ++parenthesesCount
+            } else if (token === ')') {
+                --parenthesesCount
+            }
+        }
+        return index
     }
 
     print() {
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {Node, NodeType} from './Node'
 import {Tree} from './Tree'
 
-const expr = '3+4*5'
+const expr = '3+4*((5+2)+1)'
 const tree = new Tree(expr)
-// console.log(tree)
-// tree.print()
+console.log(tree)
+tree.print()

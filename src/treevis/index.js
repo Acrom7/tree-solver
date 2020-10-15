2bcba48e31486ef264693e78212c3ef746d5d538
Acrom <zvez.dmtr@gmail.com>
2020-10-15 17:29:07 +1000

    Treevis now is a npm dependencies

--- src/treevis/index.js
+++
@@ -1,9 +0,0 @@
-const TreeNode = require('./TreeNode')
-const treeFromArray = require('./treeFromArray')
-const treeToASCII = require('./treeToASCII')
-
-module.exports = {
-  TreeNode,
-  treeFromArray,
-  treeToASCII,
-}
